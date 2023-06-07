247bbdb629dc6f94aaaf5fad8706050b37b1fcb6
taukirsheikh <taukirsheikh2@gmail.com>
2023-06-07 18:44:13 +0545

    type  any

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 // a super set of javascript
 // console.log('hellow world')
-let age: number = 20
+// let age: number = 20
 
-console.log(age)
-if (age <50)
-age+=10;
-console.log(age)
+// console.log(age)
+// if (age <50)
+// age+=10;
+// console.log(age)
+
+let slaes : number =123_456_789;
+let course : string = 'Typescript'
+// no need to anotate if the variabe is initialized
+let is_published =true
+
+//if not initialized
+let level;
